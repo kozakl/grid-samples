96871e57f937813e655f7e34d909067af098a4b6
Luke Kozak <kozakluke@gmail.com>
2018-11-11 13:02:29 +0100

    Delete sample from pages

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {Redirect, Route, Switch} from 'react-router';
 import {HashRouter} from 'react-router-dom';
 import {AlignContentSample} from '../../pages/align-content-sample';
-import {Sample} from '../../pages/sample';
 import {AreasSample} from '../../pages/areas-sample';
 import {AutoColumnsSample} from '../../pages/auto-columns-sample';
 import {BasicSample} from '../../pages/basic-sample';
@@ -21,7 +20,6 @@ export default function Content()
                     onChange={(event)=>
                         window.location.href = event.target.value}>
                     <option value="#/align-content">Align Content</option>
-                    <option value="#/sample">Sample</option>
                     <option value="#/areas">Areas</option>
                     <option value="#/auto-column">Auto Column</option>
                     <option value="#/basic-sample">Basic Sample</option>
@@ -32,7 +30,6 @@ export default function Content()
                 </select>
                 <Switch>
                     <Redirect from="/" to='/align-content' exact/>
-                    <Route path="/sample" component={Sample}/>
                     <Route path="/align-content" component={AlignContentSample}/>
                     <Route path="/areas" component={AreasSample}/>
                     <Route path="/auto-column" component={AutoColumnsSample}/>

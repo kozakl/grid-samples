6f0b330e3b3f0952bee0b271c8cfb3df5ee9f9a4
Luke Kozak <kozakluke@gmail.com>
2018-11-13 15:58:09 +0100

    Clean areas-sample

--- src/pages/areas-sample/AreasSample.tsx
+++ src/pages/areas-sample/AreasSample.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as style from './AreasSample.pcss';
 
-export default function SpinnerSample()
+export default function AreasSample()
 {
     return (
-        <div className={style.content}>
+        <div className={style.areas}>
             <div className={style.header}>HEADER</div>
             <div className={style.menu}>MENU</div>
-            <div className={style.container}>CONTAINER</div>
+            <div className={style.content}>CONTENT</div>
             <div className={style.footer}>FOOTER</div>
         </div>
     );

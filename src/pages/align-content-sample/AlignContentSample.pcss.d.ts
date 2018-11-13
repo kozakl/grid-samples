9ebea15fa69d21cc4659fa4d69c0ec6fc1054db7
Luke Kozak <kozakluke@gmail.com>
2018-11-13 15:48:18 +0100

    Clean align-content-sample

--- src/pages/align-content-sample/AlignContentSample.pcss.d.ts
+++ src/pages/align-content-sample/AlignContentSample.pcss.d.ts
@@ -1,8 +1,7 @@
-export const content:string;
+export const alignContent:string;
 export const element1:string;
 export const element2:string;
 export const element3:string;
 export const element4:string;
 export const element5:string;
 export const element6:string;
-export const element7:string;
--- src/pages/align-content-sample/AlignContentSample.tsx
+++ src/pages/align-content-sample/AlignContentSample.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as style from './AlignContentSample.pcss';
 
-export default function SpinnerSample()
+export default function AlignContentSample()
 {
     return (
-        <div className={style.content}>
+        <div className={style.alignContent}>
             <div className={style.element1}>1</div>
             <div className={style.element2}>2</div>
             <div className={style.element3}>3</div>

fcffc00b9c1a3c96e629e795046ee88d905061e9
Luke Kozak <kozakluke@gmail.com>
2018-11-14 08:35:25 +0100

    Clean items-positions-sample

--- src/pages/items-positions-sample/Content2Col.pcss.d.ts
+++ src/pages/items-positions-sample/Content2Col.pcss.d.ts
@@ -1,5 +1,5 @@
-export const content:string;
+export const itemsPositions:string;
 export const header:string;
 export const menu:string;
-export const container:string;
+export const content:string;
 export const footer:string;
--- src/pages/items-positions-sample/ItemsPositionsSample.tsx
+++ src/pages/items-positions-sample/ItemsPositionsSample.tsx
@@ -4,10 +4,10 @@ import * as style from './Content12Col.pcss';
 export default function ItemsPositionsSample()
 {
     return (
-        <div className={style.content}>
+        <div className={style.itemsPositions}>
             <div className={style.header}>HEADER</div>
             <div className={style.menu}>MENU</div>
-            <div className={style.container}>CONTAINER</div>
+            <div className={style.content}>CONTAINER</div>
             <div className={style.footer}>FOOTER</div>
         </div>
     );
